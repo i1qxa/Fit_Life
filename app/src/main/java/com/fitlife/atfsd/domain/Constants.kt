2f69cf7ea102e0a0d15a3b532e070ba2ece88374
Иван <cvb@sdf>
2024-03-15 23:19:20 +0300

    finish training screen

--- app/src/main/java/com/fitlife/atfsd/domain/Constants.kt
+++ app/src/main/java/com/fitlife/atfsd/domain/Constants.kt
@@ -3,3 +3,4 @@ package com.fitlife.atfsd.domain
 const val TYPE_CARDIO = 1
 const val TYPE_PILATES = 2
 const val TRAINING_ID = "trainingId"
+const val MILS_IN_SECOND:Long = 1000
--- app/src/main/java/com/fitlife/atfsd/ui/training/TrainingFragment.kt
+++ app/src/main/java/com/fitlife/atfsd/ui/training/TrainingFragment.kt
@@ -10,7 +10,9 @@ import androidx.lifecycle.lifecycleScope
 import androidx.navigation.NavArgs
 import androidx.navigation.fragment.navArgs
 import coil.load
+import com.fitlife.atfsd.data.local.exercise_items.ExerciseItems
 import com.fitlife.atfsd.databinding.FragmentTrainingBinding
+import com.fitlife.atfsd.domain.MILS_IN_SECOND
 import com.fitlife.atfsd.domain.TRAINING_ID
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
@@ -22,6 +24,8 @@ class TrainingFragment : Fragment() {
     private val viewModel: TrainingViewModel by viewModels()
 
     private val trainingId by lazy { arguments?.getInt(TRAINING_ID) }
+
+    private lateinit var exerciseList: List<ExerciseItems>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,56 +41,80 @@ class TrainingFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        observeExerciseList()
         setupBtnStartClickListener()
     }
 
-    private fun observeExerciseList(){
+    private fun observeExerciseList() {
         lifecycleScope.launch {
-            viewModel.exerciseList.collect{ exerciseItemsList ->
-                if (exerciseItemsList!=null){
-                    exerciseItemsList.forEach { exerciseItem ->
-                        binding.ivExerciseImg.load(exerciseItem.logo)
-                        binding.tvExerciseName.text = exerciseItem.name
-                        binding.tvExerciseDescription.text = exerciseItem.description
-                        var amountOfRepeat = exerciseItem.repeat
-                        while (amountOfRepeat>0){
-                            var timer = exerciseItem.duration
-                            updateTimer(timer)
-                            binding.timerProgress.max = timer
-                            binding.timerProgress.setProgress(timer, true)
-                                while (timer>0){
-                                delay(1000)
-                                timer-=1
-                                updateTimer(timer)
-                                    binding.timerProgress.setProgress(timer, true)
-                                if (timer ==0){
-                                    var timeForRest = 30
-                                    binding.timerProgress.max = timeForRest
-                                    binding.timerProgress.setProgress(timeForRest, true)
-                                    updateTimer(timeForRest)
-                                    while (timeForRest>0){
-                                        delay(1000)
-                                        timeForRest-=1
-                                        updateTimer(timeForRest)
-                                        binding.timerProgress.setProgress(timeForRest, true)
-                                    }
-                                }
-                            }
-                            amountOfRepeat-=1
-                        }
-                    }
+            viewModel.exerciseList.collect { exerciseItemsList ->
+                if (exerciseItemsList != null) {
+                    exerciseList = exerciseItemsList
+                    binding.ivExerciseImg.load(exerciseList[0].logo)
+                    binding.tvExerciseName.text = exerciseList[0].name
+                    binding.tvExerciseDescription.text = exerciseList[0].description
                 }
             }
         }
     }
 
-    private fun updateTimer(time:Int){
-        binding.tvTimer.text = time.toString()
+    private fun startTraining() {
+        lifecycleScope.launch {
+
+            exerciseList.forEach { exerciseItem ->
+                binding.ivExerciseImg.load(exerciseItem.logo)
+                binding.tvExerciseName.text = exerciseItem.name
+                binding.tvExerciseDescription.text = exerciseItem.description
+                var amountOfRepeat = exerciseItem.repeat
+                while (amountOfRepeat > 0) {
+                    binding.ivExerciseImg.visibility = View.VISIBLE
+                    binding.tvRest.visibility = View.GONE
+                    var timer = exerciseItem.duration
+                    updateTimer(timer)
+                    binding.layoutTimer.timerProgress.max = timer
+                    binding.layoutTimer.timerProgress.setProgress(timer, true)
+                    while (timer > 0) {
+                        delay(MILS_IN_SECOND)
+                        timer -= 1
+                        updateTimer(timer)
+                        binding.layoutTimer.timerProgress.setProgress(timer, true)
+                        if (timer == 0) {
+                            var timeForRest = 30
+                            binding.ivExerciseImg.visibility = View.GONE
+                            binding.tvRest.visibility = View.VISIBLE
+                            binding.layoutTimer.timerProgress.max = timeForRest
+                            binding.layoutTimer.timerProgress.setProgress(timeForRest, true)
+                            updateTimer(timeForRest)
+                            while (timeForRest > 0) {
+                                delay(MILS_IN_SECOND)
+                                timeForRest -= 1
+                                updateTimer(timeForRest)
+                                binding.layoutTimer.timerProgress.setProgress(timeForRest, true)
+                            }
+                        }
+                    }
+                    amountOfRepeat -= 1
+                }
+            }
+            binding.btnStart.visibility = View.VISIBLE
+        }
     }
 
-    private fun setupBtnStartClickListener(){
+    private fun updateTimer(time: Int) {
+        binding.layoutTimer.tvTimer.text = getTimeFormatted(time)
+    }
+
+    private fun getTimeFormatted(time: Int): String {
+        val mils = (time * MILS_IN_SECOND).toLong()
+        val minutes = (mils % (MILS_IN_SECOND * 60 * 60)) / (MILS_IN_SECOND * 60)
+        val seconds = time - minutes*60
+        return String.format("%02d:%02d", minutes, seconds)
+    }
+
+    private fun setupBtnStartClickListener() {
         binding.btnStart.setOnClickListener {
-            observeExerciseList()
+            startTraining()
+            binding.btnStart.visibility = View.GONE
         }
     }
 
