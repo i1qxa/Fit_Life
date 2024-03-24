b91ccc56955d0dbbae9cfb6a77dd040adc3b6e1e
Иван <cvb@sdf>
2024-03-25 00:21:50 +0300

    finish verstku

--- app/src/main/java/com/fitlife/atfsd/ui/pilates/PilatesFragment.kt
+++ app/src/main/java/com/fitlife/atfsd/ui/pilates/PilatesFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
@@ -21,6 +22,7 @@ class PilatesFragment : Fragment() {
     private val viewModel by viewModels<PilatesViewModel>()
     private val rvAdapter by lazy { TrainingRVAdapter() }
     private val recyclerView by lazy { binding.rvPilatesExercises }
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
